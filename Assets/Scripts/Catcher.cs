72037644a43abedbae7281413ac563f4d790c6a7
ravin <hewabadd@sheridancollege.ca>
2025-04-04 01:01:54 -0400

    Finished turning red and timer coroutines for both runner and catcher and death functions for both. Added movement for both with different keys for each one.

--- Assets/Scripts/Catcher.cs
+++ Assets/Scripts/Catcher.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Catcher : MonoBehaviour
 {
+    public Coroutine outsideZone;
+    private float speed = 5f;
+    public GameObject runner;
+    public SpriteRenderer sr;
+    float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        outsideZone = StartCoroutine(timer());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+    }
+
+    public IEnumerator timer()
+    {
+        while (true)
+        {
+            t += Time.deltaTime;
+            float green = sr.color.g - (0.32f * Time.deltaTime);
+            float blue = sr.color.b - (0.32f * Time.deltaTime);
+            if (sr.color.g < 0)
+            {
+                Debug.Log("Time for catcher red: " + t);
+            }
+            Color modifiedColor = new Color(sr.color.r, green, blue);
+            sr.color = modifiedColor;
+            if (t > 3.5f)
+            {
+                catcherDeath();
+                StopCoroutine(outsideZone);
+            }
+            yield return null;
+        }
+
+    }
+
+    public void catcherDeath()
+    {
+        Debug.Log("Catcher died!");
     }
 }
--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -5,30 +5,60 @@ using UnityEngine.Events;
 
 public class Runner : MonoBehaviour
 {
-    
     Coroutine outsideZone;
+    SpriteRenderer sr;
+    float speed = 5f;
     float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+       sr = gameObject.GetComponent<SpriteRenderer>();
+       outsideZone = StartCoroutine(timer());
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
 
     }
 
-    IEnumerator timer()
+    public IEnumerator timer()
     {
-        t += Time.deltaTime;
-        if (t> 2)
+        while (true)
         {
-            runnerDeath();
+            t += Time.deltaTime;
+            float green = sr.color.g - (0.24f * Time.deltaTime);
+            float blue = sr.color.b - (0.24f * Time.deltaTime);
+            if (sr.color.g < 0)
+            {
+                Debug.Log("Time for become red: " + t);
+            }
+            Color modifiedColor = new Color(sr.color.r, green, blue);
+            sr.color =modifiedColor;
+            if (t > 5)
+            {
+                runnerDeath();
+                StopCoroutine(outsideZone);
+            }
+            yield return null;
         }
-        yield return null;
+        
     }
 
     public void runnerDeath()
